ce25687b2edeb6cc6d0a9f47b147bdd8f4cf69b7
Flavio Max Franco Tunqui <flavio.franco@globant.com>
2019-12-20 19:03:17 -0500

    feat(*): database added

---
+++ app/src/main/java/com/frantun/androidroom_kotlin/dao/WordDao.kt
@@ -0,0 +1,20 @@
+package com.frantun.androidroom_kotlin.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.frantun.androidroom_kotlin.entities.Word
+
+@Dao
+interface WordDao {
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    fun getAlphabetizedWords(): LiveData<List<Word>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(word: Word)
+
+    @Query("DELETE FROM word_table")
+    suspend fun deleteAll()
+}
